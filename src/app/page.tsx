ea479e70388c39233c5e7439c2609a6147249517
Esdras Nani <esdraslnani@hotmail.com>
2023-12-24 17:46:38 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <p className="text-4xl font-bold">Hello World</p>
+    </div>
+  )
+}
